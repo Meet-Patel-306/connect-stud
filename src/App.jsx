bce2b39c36d58fd7842d6761b592fc3cae5eb438
Meet Patel <meetpt306@gmail.com>
2025-01-16 19:21:04 +0530

    setup react and tailwind

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Navbar from "./components/Navbar"
+
+function App() {
+  return (
+    <>
+      <Navbar/>
+      <h1 className="text-3xl font-bold underline">hello</h1>
+    </>
+  )
+}
+
+export default App
