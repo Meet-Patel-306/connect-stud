0472dbdf4aaf129046063531cce5391c890b6a58
akshay395 <395akshay@gmail.com>
2025-01-29 13:38:29 +0530

    Login(Extended)

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from "react";
 import Navbar from "./components/Navbar";
 import Profile from "./components/Profile/Profile";
-
+import Login from "./components/Login/Login";
 function App() {
   return (
     <>
       <Navbar />
       <h1 className="text-3xl font-bold underline">hello Mohit Swami</h1>
       <Profile />
+      <Login />
     </>
   );
 }
