c2ed7097d8a1f017fb9b02eb62fcf6b9f1df8fb2
Meet Patel <meetpt306@gmail.com>
2025-03-24 19:14:26 +0530

    error 404 style change

--- src/components/404/Error.jsx
+++ src/components/404/Error.jsx
@@ -1,37 +1,75 @@
 import "../../error.css";
-import error2 from "../../../public/404.png"
+import error2 from "../../../public/404.png";
 
 export default function Error() {
-    return (                
-        <div className="stars bg-purple">
-            <div className="custom-navbar">
-                <div className="brand-logo">
-                    <img src="http://salehriaz.com/404Page/img/logo.svg" width="80px" alt="Logo"/>
-                </div>
+  return (
+    <>
+      <div className="bg-purple">
+        <div className="stars">
+          <div className="custom-navbar">
+            <div className="brand-logo">
+              <img
+                src="http://salehriaz.com/404Page/img/logo.svg"
+                width="80px"
+                alt="Logo"
+              />
             </div>
-            <div className="central-body">
-                <img className="image-404" src="http://salehriaz.com/404Page/img/404.svg" width="300px" alt="404"/>
-                <a href="http://salehriaz.com/404Page/404.html" className="btn-go-home" target="_blank" rel="noopener noreferrer">
-                    GO BACK HOME
-                </a>
+          </div>
+          <div className="central-body">
+            <img
+              className="image-404"
+              src="http://salehriaz.com/404Page/img/404.svg"
+              width="300px"
+              alt="404"
+            />
+            <a
+              href="http://salehriaz.com/404Page/404.html"
+              className="btn-go-home"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              GO BACK HOME
+            </a>
+          </div>
+          <div className="objects">
+            <img
+              className="object_rocket"
+              src="http://salehriaz.com/404Page/img/rocket.svg"
+              width="40px"
+              alt="Rocket"
+            />
+            <div className="earth-moon">
+              <img
+                className="object_earth"
+                src="http://salehriaz.com/404Page/img/earth.svg"
+                width="100px"
+                alt="Earth"
+              />
+              <img
+                className="object_moon"
+                src="http://salehriaz.com/404Page/img/moon.svg"
+                width="80px"
+                alt="Moon"
+              />
             </div>
-            <div className="objects">
-                <img className="object_rocket" src="http://salehriaz.com/404Page/img/rocket.svg" width="40px" alt="Rocket"/>
-                <div className="earth-moon">
-                    <img className="object_earth" src="http://salehriaz.com/404Page/img/earth.svg" width="100px" alt="Earth"/>
-                    <img className="object_moon" src="http://salehriaz.com/404Page/img/moon.svg" width="80px" alt="Moon"/>
-                </div>
-                <div className="box_astronaut">
-                    <img className="object_astronaut" src="http://salehriaz.com/404Page/img/astronaut.svg" width="140px" alt="Astronaut"/>
-                </div>
+            <div className="box_astronaut">
+              <img
+                className="object_astronaut"
+                src="http://salehriaz.com/404Page/img/astronaut.svg"
+                width="140px"
+                alt="Astronaut"
+              />
             </div>
-            <div className="glowing_stars">
-                <div className="star"></div>
-                <div className="star"></div>
-                <div className="star"></div>
-                <div className="star"></div>
-                <div className="star"></div>
-            </div>
+          </div>
+          <div className="glowing_stars">
+            <div className="star"></div>
+            <div className="star"></div>
+            <div className="star"></div>
+            <div className="star"></div>
+            <div className="star"></div>
+          </div>
         </div>
-    );
+      </div>
+    </>
+  );
 }
