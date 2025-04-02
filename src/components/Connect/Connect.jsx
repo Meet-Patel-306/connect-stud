821f235cc30af5e04dfa9e50b6bb898cad7b20b5
Mohit Pipaliya <mohitpipaliya2005@gmail.com>
2025-04-02 22:34:55 +0530

    create UserCard

--- src/components/Connect/Connect.jsx
+++ src/components/Connect/Connect.jsx
@@ -1,10 +1,24 @@
-import Navbar from "../Navbar";
+import UserCard from "./UserCard";
+
 export default function Connect() {
+  const users = Array(22).fill(null); // Adjust number of cards dynamically
+
   return (
-    <>
-      <h1 className="text-3xl font-bold underline text-red-600 dark:text-gray-950">
-        connect
-      </h1>
-    </>
+    <section aria-labelledby="connect-title" className="flex flex-col items-center bg-gray-50 dark:bg-gray-800">
+      <header className="py-4 lg:py-8 w-full flex justify-center">
+        <h1 id="connect-title" className="text-3xl font-bold text-gray-800 dark:text-gray-200">
+          Connect With Us
+        </h1>
+      </header>
+
+      <div className="container mx-auto bg-gray-50 dark:bg-gray-800 w-full">
+        <div className="justify-center grid gap-y-10 gap-x-10 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4">
+          {users.map((_, index) => (
+            <UserCard key={index} />
+          ))}
+        </div>
+      </div>
+    </section>
+
   );
 }
